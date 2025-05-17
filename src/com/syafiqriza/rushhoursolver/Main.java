8393adb641a446e02d679475b1152f8962ed7402
L4mbads <rizacal.mamen@gmail.com>
2025-05-17 13:33:15 +0700

    refactor: separate main with views

--- src/com/syafiqriza/rushhoursolver/Main.java
+++ src/com/syafiqriza/rushhoursolver/Main.java
@@ -1,35 +1,16 @@
 package com.syafiqriza.rushhoursolver;
 
-import com.syafiqriza.rushhoursolver.model.*;
-
-import java.io.IOException;
-import java.util.Scanner;
+import com.syafiqriza.rushhoursolver.view.CLI;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String filePath;
-
-        if (args.length == 0) {
-            System.out.print("Masukkan path ke file puzzle (.txt): ");
-            filePath = sc.nextLine();
+        if (args.length > 0 && args[0].equalsIgnoreCase("gui")) {
+            System.out.println("GUI anjay gurinjay");
+            // GUI gui = new GUI();
+            // gui.run();
         } else {
-            filePath = args[0];
-        }
-
-        try {
-            Board board = Utils.readRushHourPuzzleFromFile(filePath);
-            board.printBoard();
-            System.out.println("- Tujuan: (" + board.getGoalRow() + ", " + board.getGoalCol() + ")");
-
-        } catch (IOException e) {
-            System.err.println("File tidak ditemukan: " + e.getMessage());
-        } catch (IllegalArgumentException e) {
-            System.err.println("Puzzle tidak valid: " + e.getMessage());
-        } catch (Exception e) {
-            System.err.println("Error: " + e.getMessage());
-        } finally {
-            sc.close();
+            CLI cli = new CLI();
+            cli.run();
         }
     }
 }
